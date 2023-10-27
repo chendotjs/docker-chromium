322ee7deaf5f8f4bba33edd7e3e8ab4371120a8e
chendotjs <chendotjs@qq.com>
2023-10-27 18:16:12 +0800

    update

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,4 +6,4 @@ set -u # Treat unset variables as an error.
 export HOME=/config
 mkdir -p /config/profile
 whoami
-exec /usr/bin/firefox_wrapper --user-data-dir=/config/profile --force-dark-mode >> /config/log/chromium/output.log 2>> /config/log/chromium/error.log
+exec /usr/bin/firefox_wrapper --no-sandbox --user-data-dir=/config/profile --force-dark-mode >> /config/log/chromium/output.log 2>> /config/log/chromium/error.log
